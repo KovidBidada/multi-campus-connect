808b2113afc3371d6028c3a4ca4172f752476b74
Mohammed Mustafa Khan <mustafak05062@gmail.com>
2024-11-30 22:41:01 +0530

    firebase url bug

--- src/app/firebase/firebaseConfig.jsx
+++ src/app/firebase/firebaseConfig.jsx
@@ -7,7 +7,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env. NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  projectId: "campusconnect-3d30b",
   databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
