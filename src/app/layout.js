49e0723db157dc8f4f43ba73e667f50d1aeb14e8
Mohammed Mustafa Khan <mustafak05062@gmail.com>
2024-12-01 17:52:23 +0530

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Analytics/>
         <Navbar/>
         {children}
         <Footer/>
