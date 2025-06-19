4c2a1b82b7b5dcbb76899da02a8bbfc8d22ecfb5
KovidBidada <kovidb186@gmail.com>
2025-06-19 23:37:17 +0530

    Update page.js

--- src/app/forum/page.js
+++ src/app/forum/page.js
@@ -217,7 +217,7 @@ function ForumPage() {
                   type="submit"
                   className="mt-2 bg-green-600 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-green-700 transition-all duration-300 ease-in-out"
                 >
-                  Add Comment
+                  Add Comment<br>
                 </button>
               </form>
             )}
